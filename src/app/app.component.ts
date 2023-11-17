9f7b1177a8616f378b60335f65f146d2e34d19e0
ahmetsyk <oslosahmet@gmail.com>
2023-11-17 10:54:39 +0300

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'project1';
+  title:string = 'Home Page';
+  pageName:string = 'Initial page';
+  name?:string;
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
+  @Input() title?: string;
+  @Input() pageName?: string;
+  @Input() name?:string;
 }
